aaa5250a6c32891c49d023d49fb18632c1268322
cihangemi <83552885+cihangemi@users.noreply.github.com>
2023-02-13 22:31:21 +0300

    modul ornek

---
+++ 4İşlem/calc.py
@@ -0,0 +1,12 @@
+def topla(x,y):
+     sonuc = x+y
+     return print(sonuc)
+def cikar(x,y):
+     sonuc = x-y
+     return print(sonuc)
+def bol(x,y):
+     sonuc = x/y
+     return print(sonuc)
+def carp(x,y):
+     sonuc = x*y
+     return print(sonuc)
---
+++ 4İşlem/main.py
@@ -0,0 +1,29 @@
+import calc
+
+print("Calculator v0.001")
+print("+:Toplama\n-:Çıkarma\n/:Bölme\n*:Çarpma")
+
+while(True):
+    n1 = int(input("1.sayi:"))
+    n2 = int(input("2.sayi:"))
+    choose=input("Yapmak istediğiniz işlemi yukarıda belirtilen şekilde seiçiniz:")
+
+    if choose == '+':
+        calc.topla(n1,n2)
+    elif choose == '-':
+        calc.cikar(n1,n2)
+    elif choose == '/':
+        calc.bol(n1,n2)
+    elif choose == '*':
+        calc.carp(n1,n2)
+    else:
+        print("Hatalı giriş\nTekrar girin")
+    onay = input("Devam etmek İster misiniz ? E/H\n :")
+    if onay == 'e' or onay == 'E':
+        continue
+    else:
+        print("İyi Günler")
+        break
+
+
+
