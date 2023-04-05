bb6663c92100052ebf120fc46e59c0193280a1d4
cihangemi <83552885+cihangemi@users.noreply.github.com>
2023-04-05 08:52:30 +0300

    NTP ilk adım

--- OOP/enemy.py
+++ OOP/enemy.py
@@ -1,22 +1,48 @@
+
 "_init_ nesneler yaratılınca ilk çağrılan ilk fonksiyon"
+
+import random
 class Dusman:
-    def __init__(self,isim='Ganxboy',kalan_can=100,saldiri_gücü=15,mermi_sayisi=21):
+    def __init__(self,isim,kalan_can,saldiri_gücü,mermi_sayisi,atis=random.randrange(10,20)):
         self.isim=isim
         self.kalan_can=kalan_can
         self.saldiri_gücü=saldiri_gücü
         self.mermi_sayisi=mermi_sayisi
+        self.atis=atis
     def print(self):
-        print("İnfo")
-        print(self.isim)
-        print(self.kalan_can)
-        print(self.saldiri_gücü)
-        print(self.mermi_sayisi)
+        print("İnfo\n")
+        print("Ad:"+str(self.isim))
+        print("Can:"+str(self.kalan_can))
+        print("Power:"+str(self.saldiri_gücü))
+        print("Ammo:"+str(self.mermi_sayisi))
+        print("------------------")
+    def Saldir(self):
+        print(self.isim+"--->Ştrauvv")
 
-dusman=Dusman()
-print("Default Düşman")
-dusman.print()
-dusman1=Dusman("Cartcix",31,100,1)
-print("Manuel Düşman xD")
-dusman1.print()
+        print(str(self.atis)+"Mermi attın")
+        self.mermi_sayisi -= self.atis
+        return (self.atis,self.saldiri_gücü)
+    def Vurul(self,atis,saldiri_gücü):
+        print(self.isim+": Ah galbimmm.:(")
+        self.kalan_can -= (atis * saldiri_gücü)
+    def AmmoSayisi(self):
+        if(self.mermi_sayisi < 0):
+            print(self.isim+": Mermi bitti mermi")
+
+        elif(self.mermi_sayisi > 0):
+            print(self.isim+":"+str(self.mermi_sayisi)+"Mermi kaldı")
+    def Yasiormu(self):
+        if(self.kalan_can < 0):
+            print("Topragı bol olsun")
+        elif(self.kalan_can > 0):
+            print(str(self.kalan_can)+"Canım var hala xD")
+
+
+
+
+
+
+
+
 
 
